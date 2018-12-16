5d1b3bd8775f775082edd19a666f300b9dc42e8e
Yamato Ashikaga <a.yamato.so.ri.00@gmail.com>
2018-12-16 15:49:37 +0900

    feat: Cacheから検索する処理を追加

--- app/Persistence/FileCacheEventRepository.php
+++ app/Persistence/FileCacheEventRepository.php
@@ -7,51 +7,31 @@ use App\Domain\Model\Event;
 
 class FileCacheEventRepository implements EventRepositoryInterface
 {
+
+    const CACHE_KEY_EVENTS = 'events';
+
     public function events()
     {
-
-        $event1 = new Event();
-        $event1->id = '110977';
-        $event1->eventDate = '2018-10-10';
-        $event1->title = '#YYPHP #64【PHPの情報交換・ワイワイ話そう・仲間作り・ゆるめ・にぎやかめ】';
-        $event1->eventPageUrl = 'https://yyphp.connpass.com/event/110977/';
-        $event1->participants = 6;
-
-        $event2 = new Event();
-        $event2->id = '110978';
-        $event2->eventDate = '2018-10-11';
-        $event2->title = '#YYPHP #64【PHPの情報交換・ワイワイ話そう・仲間作り・ゆるめ・にぎやかめ】';
-        $event2->eventPageUrl = 'https://yyphp.connpass.com/event/110978/';
-        $event2->participants = 6;
-
-        $event3 = new Event();
-        $event3->id = '110979';
-        $event3->eventDate = '2018-10-12';
-        $event3->title = '#YYPHP #64【PHPの情報交換・ワイワイ話そう・仲間作り・ゆるめ・にぎやかめ】';
-        $event3->eventPageUrl = 'https://yyphp.connpass.com/event/110979/';
-        $event3->participants = 6;
-
-        return [
-            $event1,
-            $event2,
-            $event3
-        ];
+        return Cache::get(self::CACHE_KEY_EVENTS);
     }
 
-    public function eventById($id)
+    public function eventById($eventId)
     {
-        $event1 = new Event();
-        $event1->id = $id;
-        $event1->eventDate = '2018-10-10';
-        $event1->title = '#YYPHP #64【PHPの情報交換・ワイワイ話そう・仲間作り・ゆるめ・にぎやかめ】';
-        $event1->eventPageUrl = 'https://yyphp.connpass.com/event/110977/';
-        $event1->participants = 6;
+        $events = $this->events();
 
-        return $event1;
+        foreach($events as $key => $event)
+        {
+            if($event->id === $eventid)
+            {
+                return $event;
+            }
+        }
+
+        return null;
     }
 
     public function saveEvents($events)
     {
-        Cache::forever('events', $events);
+        Cache::forever(self::CACHE_KEY_EVENTS, $events);
     }
 }
--- app/Persistence/FileCacheParticipantRepository.php
+++ app/Persistence/FileCacheParticipantRepository.php
@@ -7,33 +7,29 @@ use App\Domain\Model\Participant;
 
 class FileCacheParticipantRepository implements ParticipantRepositoryInterface
 {
+
+    const CACHE_KEY_PARTICIPANTS = 'participants';
+
     public function participantsByEventId($eventId)
     {
-        $participant1 = new Participant();
-        $participant1->frame = 'スタッフ枠';
-        $participant1->name = 'スタッフ枠 1';
-        $participant1->iconUrl = 'https://connpass-tokyo.s3.amazonaws.com/thumbs/57/4c/574c94930d8e233ca312c0a060114ab6.png';
+        $participants = Cache::get(self::CACHE_KEY_PARTICIPANTS);
 
-        $participant2 = new Participant();
-        $participant2->frame = '現地参加';
-        $participant2->name = '現地参加 1';
-        $participant2->iconUrl = 'https://connpass-tokyo.s3.amazonaws.com/thumbs/57/4c/574c94930d8e233ca312c0a060114ab6.png';
+        $resultParticipants = [];
 
-        $participant3 = new Participant();
-        $participant3->frame = 'リモート枠';
-        $participant3->name = 'リモート枠 1';
-        $participant3->iconUrl = 'https://connpass-tokyo.s3.amazonaws.com/thumbs/57/4c/574c94930d8e233ca312c0a060114ab6.png';
+        foreach($participants as $key => $participant)
+        {
+            if($participant->eventId === $eventid)
+            {
+                $resultParticipants[] = $participant;
+            }
+        }
 
-
-        return [
-            $participant1,
-            $participant2,
-            $participant3
-        ];
+        
+        return $resultParticipants;
     }
 
     public function saveParticipants($participants)
     {
-        Cache::forever('participants', $participants);
+        Cache::forever(self::CACHE_KEY_PARTICIPANTS, $participants);
     }
 }
